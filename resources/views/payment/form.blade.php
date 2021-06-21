ec99021949e5ca29405f65934d3c68e8df4c94c6
insame.kz <ersatama@gmail.com>
2021-06-21 15:32:09 +0600

    fixed api

--- resources/views/payment/form.blade.php
+++ resources/views/payment/form.blade.php
@@ -1,4 +1,4 @@
-<form action="{{$booking['payment_url']}}" method="post" style="visibility: hidden;">
+<form action="https://api.paybox.money/v1/merchant/538109/card/pay" method="post" style="visibility: hidden;">
     <input type="text" placeholder="pg_merchant_id" name="pg_merchant_id" value="{{$payment['pg_merchant_id']}}" required><br><br>
     <input type="text" placeholder="pg_payment_id" name="pg_payment_id" value="{{$payment['pg_payment_id']}}" required><br><br>
     <input type="text" placeholder="pg_salt" name="pg_salt" value="{{$payment['pg_salt']}}" required><br><br>

42da49161131e9eb2f48fcdae561505d763bd757
insame.kz <ersatama@gmail.com>
2021-06-29 18:03:42 +0600

    fixed api

--- resources/js/components/footerMenu/FooterMenu.vue
+++ resources/js/components/footerMenu/FooterMenu.vue
@@ -3,25 +3,25 @@
         <div class="row bg-white width-auto footer-fixed d-flex justify-content-center">
             <div class="col-12 col-md-4 p-0">
                 <div class="footer-fixed-main w-100">
-                    <a href="/home" class="p-0 w-25">
+                    <a href="/home" class="p-0 py-1 w-25">
                         <button class="btn font-weight-bold w-100">
                             <img src="/img/logo/home.svg" width="20" height="20">
                             <div class="title">Категории</div>
                         </button>
                     </a>
-                    <a href="/top" class="p-0 w-25">
+                    <a href="/top" class="p-0 py-1 w-25">
                         <button class="btn font-weight-bold w-100">
                             <img src="/img/logo/star.svg" width="20" height="20">
                             <div class="title">Топ</div>
                         </button>
                     </a>
-                    <a href="/favorite" class="p-0 w-25">
+                    <a href="/favorite" class="p-0 py-1 w-25">
                         <button class="btn font-weight-bold w-100">
                             <img src="/img/logo/favorite.svg" width="20" height="20">
                             <div class="title">Избранное</div>
                         </button>
                     </a>
-                    <a href="/profile" class="p-0 w-25">
+                    <a href="/profile" class="p-0 py-1 w-25">
                         <button class="btn font-weight-bold w-100">
                             <img src="/img/logo/profile.svg" width="20" height="20">
                             <div class="title">Профиль</div>

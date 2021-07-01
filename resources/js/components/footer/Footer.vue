3e5f4d5cd510a26c7f1f17c0b4fced38a305213e
insame.kz <ersatama@gmail.com>
2021-07-01 18:56:34 +0600

    fixed

--- resources/js/components/footer/Footer.vue
+++ resources/js/components/footer/Footer.vue
@@ -3,19 +3,6 @@
         <div class="container pb-5 px-0">
             <div class="row mb-5">
                 <div class="col-12 col-md-6 col-lg-3">
-                    <h3 class="font-weight-bold footer-title">Reserved</h3>
-                    <h6 class="mt-2 mt-md-3 footer-title-sub">Скачайте приложение</h6>
-                    <div class="footer-mobile-main">
-                        <div class="mt-1 mt-md-4 main-thumbnail overflow-hidden footer-mobile">
-                            <img src="/img/main/img-11.png" class="img-thumbnail bg-white p-0 footer-mobile-icon">
-                        </div>
-                        <div class="mt-1 mt-md-3 main-thumbnail overflow-hidden footer-mobile">
-                            <img src="/img/main/img-10.png" class="img-thumbnail bg-white p-0 footer-mobile-icon">
-                        </div>
-                    </div>
-                    <h6 class="pt-3 mt-md-4 footer-company">©  Reserved 2020 - 2021</h6>
-                </div>
-                <div class="col-12 col-md-6 col-lg-3">
                     <h5 class="font-weight-bold mt-3 mt-md-4 footer-menu-title">Давайте сделаем это вместе</h5>
                     <ul class="list-group p-0 mt-md-4 footer-menu-group">
                         <li class="list-group-item bg-transparent border-0 px-0 py-1 py-md-2">Для ресторанов</li>
@@ -42,6 +29,19 @@
                         <li class="list-group-item bg-transparent border-0 px-0 py-2">Twitter</li>
                         <li class="list-group-item bg-transparent border-0 px-0 py-2">LinkedIn</li>
                     </ul>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <h3 class="font-weight-bold footer-title">Reserved</h3>
+                    <h6 class="mt-3 footer-title-sub">Скачайте приложение</h6>
+                    <div class="footer-mobile-main">
+                        <div class="mt-1 mt-md-4 main-thumbnail overflow-hidden footer-mobile">
+                            <img src="/img/main/img-11.png" class="img-thumbnail bg-white p-0 footer-mobile-icon">
+                        </div>
+                        <div class="mt-1 mt-md-3 main-thumbnail overflow-hidden footer-mobile">
+                            <img src="/img/main/img-10.png" class="img-thumbnail bg-white p-0 footer-mobile-icon">
+                        </div>
+                    </div>
+                    <h6 class="pt-3 mt-md-4 footer-company">©  Reserved 2020 - 2021</h6>
                 </div>
             </div>
         </div>

e10aedd55ff3600dff9a16ddd57ccbc0cf98a511
insame.kz <ersatama@gmail.com>
2021-07-15 04:21:28 +0600

    fixed api payment

--- resources/views/vendor/backpack/base/card/sections.blade.php
+++ resources/views/vendor/backpack/base/card/sections.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         @foreach($organizationTableListService->getByTableId($section->id) as &$table)
             {{$table->id.'='.$date}}
-            {{json_encode($bookingService->getLastByTableId($table->id, $date))}}
+            {{json_encode($bookingService->getLastByTableId($table->id, $date, $organization->timezone))}}
             <div class="col-xl-2 col-lg-4 col-md-6 col-6">
                 @include('vendor.backpack.base.card.card',[
                     'table' =>  $table,

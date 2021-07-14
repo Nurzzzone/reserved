cacf48942bad98b3325fc175ccc7af8c278590f6
insame.kz <ersatama@gmail.com>
2021-07-15 03:29:03 +0600

    fixed api payment

--- resources/views/vendor/backpack/base/card/sections.blade.php
+++ resources/views/vendor/backpack/base/card/sections.blade.php
@@ -1,5 +1,6 @@
 @foreach($organizationTableService->getByOrganizationId($organization->id) as & $section)
     <h3 class="my-3">{{$section->name}}</h3>
+    {{$date}}
     <div class="row">
         @foreach($organizationTableListService->getByTableId($section->id) as &$table)
             <div class="col-xl-2 col-lg-4 col-md-6 col-6">

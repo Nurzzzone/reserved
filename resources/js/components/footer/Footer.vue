a2dd45c2e1e48e7bd87d19327ab879b3c5dda182
ersa <ersatama@gmail.com>
2021-08-10 19:27:42 +0600

    organization request

--- resources/js/components/footer/Footer.vue
+++ resources/js/components/footer/Footer.vue
@@ -10,12 +10,18 @@
                                 Для ресторанов
                             </a>
                         </li>
-
-                        <li class="list-group-item bg-transparent border-0 px-0 py-1 py-md-2">Для компании</li>
-                        <li class="list-group-item bg-transparent border-0 px-0 py-1 py-md-2">Вакансии</li>
+                        <li class="list-group-item bg-transparent border-0 px-0 py-1 py-md-2">
+                            <a class="p-0 m-0 text-white">
+                                Контакты
+                            </a>
+                        </li>
+                        <li class="list-group-item bg-transparent border-0 px-0 py-1 py-md-2">Поддержка</li>
+                        <li class="list-group-item bg-transparent border-0 px-0 py-1 py-md-2">
+                            <h6 class="pt-3 mt-md-4 footer-company">©  Reserved 2020 - 2021</h6>
+                        </li>
                     </ul>
                 </div>
-                <div class="col-12 col-md-6 col-lg-3">
+<!--                <div class="col-12 col-md-6 col-lg-3">
                     <h5 class="font-weight-bold mt-3 mt-md-4 footer-menu-title">Ссылки Reserved</h5>
                     <ul class="list-group p-0 mt-md-4 footer-menu-group">
                         <li class="list-group-item bg-transparent border-0 px-0 py-1 py-md-2">Контакты</li>
@@ -24,8 +30,8 @@
                         <li class="list-group-item bg-transparent border-0 px-0 py-1 py-md-2">О нас</li>
                         <li class="list-group-item bg-transparent border-0 px-0 py-1 py-md-2">Блог</li>
                     </ul>
-                </div>
-                <div class="col-12 col-md-6 col-lg-3">
+                </div>-->
+<!--                <div class="col-12 col-md-6 col-lg-3">
                     <h5 class="font-weight-bold mt-3 mt-md-4 footer-menu-title">Мы в соц-сетях</h5>
                     <ul class="list-group p-0 mt-md-4 footer-menu-group">
                         <li class="list-group-item bg-transparent border-0 px-0 py-2">Facebook</li>
@@ -34,8 +40,8 @@
                         <li class="list-group-item bg-transparent border-0 px-0 py-2">Twitter</li>
                         <li class="list-group-item bg-transparent border-0 px-0 py-2">LinkedIn</li>
                     </ul>
-                </div>
-                <div class="col-12 col-md-6 col-lg-3">
+                </div>-->
+<!--                <div class="col-12 col-md-6 col-lg-3">
                     <h3 class="font-weight-bold footer-title">Reserved</h3>
                     <h6 class="mt-3 footer-title-sub">Скачайте приложение</h6>
                     <div class="footer-mobile-main">
@@ -47,7 +53,7 @@
                         </div>
                     </div>
                     <h6 class="pt-3 mt-md-4 footer-company">©  Reserved 2020 - 2021</h6>
-                </div>
+                </div>-->
             </div>
         </div>
     </div>

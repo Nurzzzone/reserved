a1d23983205484d923a7d988bd5ed9c7f49e5577
ersa <ersatama@gmail.com>
2021-08-23 23:09:10 +0600

    fixed

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,6 +10,7 @@ use App\Services\OrganizationTableList\OrganizationTableListService;
 use App\Services\Booking\BookingService;
 use App\Services\User\UserService;
 use App\Services\Category\CategoryService;
+use App\Services\WebTraffic\WebTrafficService;
 use App\Jobs\WebTraffic;
 
 class MainController extends Controller
@@ -23,13 +24,14 @@ class MainController extends Controller
     protected $categoryService;
     protected $webTrafficService;
 
-    public function __construct(OrganizationService $organizationService, OrganizationTableService $organizationTableService, OrganizationTableListService $organizationTableListService, BookingService $bookingService, UserService $userService, CategoryService $categoryService) {
+    public function __construct(OrganizationService $organizationService, OrganizationTableService $organizationTableService, OrganizationTableListService $organizationTableListService, BookingService $bookingService, UserService $userService, CategoryService $categoryService, WebTrafficService $webTrafficService) {
         $this->organizationService  =   $organizationService;
         $this->organizationTableService =   $organizationTableService;
         $this->organizationTableListService =   $organizationTableListService;
         $this->bookingService   =   $bookingService;
         $this->userService  =   $userService;
         $this->categoryService  =   $categoryService;
+        $this->webTrafficService    =   $webTrafficService;
     }
 
     public function dashboard() {
